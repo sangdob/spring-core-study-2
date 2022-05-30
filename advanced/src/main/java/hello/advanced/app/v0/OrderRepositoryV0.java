dddee9fd8b914ece3318c31d671e9e99db621dad
sangdob <51118110+sangdob@users.noreply.github.com>
2022-05-31 01:56:03 +0900

    order라는 형태로 스프링 구조 테스트 및 기본적인 형태 시작

--- advanced/src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
+++ advanced/src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -1,4 +1,25 @@
+package hello.advanced.app.v0;
 
-public class OrderRespositoryV0 {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        if (itemId.equals("ex")) {
+            throw new IllegalArgumentException("Exception!! ");
+        }
+
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.getMessage();
+        }
+    }
 }
