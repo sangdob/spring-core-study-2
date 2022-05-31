0ad556177bcea79fc9a1874797d79efcb9e0ff9c
sangdob <51118110+sangdob@users.noreply.github.com>
2022-05-31 23:58:53 +0900

    order라는 형태로 스프링 구조 테스트 및 기본적인 형태 시작

--- advanced/src/main/java/hello/advanced/app/v0/OrderController.java
+++ advanced/src/main/java/hello/advanced/app/v0/OrderController.java
@@ -1,10 +1,13 @@
 package hello.advanced.app.v0;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@Controller + @ResponseBody
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderServiceV0 orderServiceV0;
