9b9d6665c32ff221a4eefa486a8424b5e9f5b6fd
sangdob <51118110+sangdob@users.noreply.github.com>
2022-06-11 14:18:54 +0900

    ---- proxy 패턴 예제 시작 commit ----
    주석 추가

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -5,6 +5,9 @@ import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+/*
+* scanBasePackages를 통하여 컴포넌트 스캔을 지정하여 할 수 있도록 가능함.
+* */
 //@Import({AppV1Config.class})
 @Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
