d985956730d6619d30af490109eadc2c2251b38b
sangdob <51118110+sangdob@users.noreply.github.com>
2022-06-16 23:37:06 +0900

    ---- proxy 패턴 예제 시작 commit ----
    프록시가 연결되지 않은 concretelogic 예제 작성

---
+++ proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
@@ -0,0 +1,16 @@
+package hello.proxy.pureproxy.concreteproxy.code;
+
+import org.junit.jupiter.api.Test;
+
+public class ConcreteClient {
+
+    private final ConcreteLogic concreteLogic;
+
+    public ConcreteClient(ConcreteLogic concreteLogic) {
+        this.concreteLogic = concreteLogic;
+    }
+
+    public void execute() {
+        concreteLogic.operation();
+    }
+}
