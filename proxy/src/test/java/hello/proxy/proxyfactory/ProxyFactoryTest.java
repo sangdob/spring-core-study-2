6e27bd69ff9f9e8dc9f941363cc919dc0ddb3ad0
snagdob <skql1358@gmail.com>
2022-06-24 21:33:21 +0900

    ProxyFactory 예제
    Aop를 활용한 advice Interface활용 및 주입
    
    테스트코드 작성 테스팅

---
+++ proxy/src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -0,0 +1,32 @@
+package hello.proxy.proxyfactory;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+public class ProxyFactoryTest {
+
+    @Test
+    @DisplayName("If you have interface, then use JDK dynamic Proxy")
+    void interfaceProxy() {
+        ServiceImpl target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("target class = {}", target.getClass());
+        log.info("proxy class = {}", proxy.getClass());
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+}
