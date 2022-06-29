537ed19a80e9c31a748a4b8e769a30614a6242d9
sangdob <51118110+sangdob@users.noreply.github.com>
2022-06-29 22:06:18 +0900

    -------스프링이 지원하는 프록시------
    rpxoyfactory advisor interface 형식의 구조 활용

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,7 @@
 package hello.proxy;
 
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -15,7 +16,8 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import({InterfaceProxyConfig.class})
 //@Import({ConcreteProxyConfig.class})
-@Import({DynamicProxyBasicConfig.class})
+//@Import({DynamicProxyBasicConfig.class})
+@Import({ProxyFactoryConfigV1.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 	public static void main(String[] args) {
