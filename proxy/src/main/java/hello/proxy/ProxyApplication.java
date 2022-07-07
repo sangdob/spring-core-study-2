a86d68c882aef0fbf9f768c0bfff878948e59972
sangdob <51118110+sangdob@users.noreply.github.com>
2022-07-07 23:03:45 +0900

    -------스프링이 지원하는 프록시------
    AOP autoproxy적용

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -3,6 +3,7 @@ package hello.proxy;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -19,8 +20,8 @@ import org.springframework.context.annotation.Import;
 //@Import({ConcreteProxyConfig.class})
 //@Import({DynamicProxyBasicConfig.class})
 //@Import({ProxyFactoryConfigV1.class})
-@Import(BeanPostProcessorConfig.class)
-
+//@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 	public static void main(String[] args) {
